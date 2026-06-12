4981eca5e4ba5fd343534f4df8c626849a075344
Mohamed Anwar <mhmdwaelanwr@gmail.com>
2026-06-12 21:56:21 +0300

    feat: add diagnostic feature enum

---
+++ src/PadScope.Core/Testing/DiagnosticFeature.cs
@@ -0,0 +1,19 @@
+namespace PadScope.Core.Testing;
+
+public enum DiagnosticFeature
+{
+    DeviceDiscovery,
+    ProfileMatching,
+    ReportExport,
+    HidIdentityInspection,
+    InputVisualization,
+    RumbleTest,
+    LightbarTest,
+    TouchpadTest,
+    GyroTest,
+    BluetoothReconnectDiagnostics,
+    HidHideDiagnostics,
+    WindowsAudioEndpointCheck,
+    Ds4AudioProbe,
+    Ds4AudioStreaming
+}
